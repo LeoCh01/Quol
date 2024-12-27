ad81c296d633abc5246beecd74a9436aa3f38a78
Leo Cheng <leocheng333375749@gmail.com>
2024-12-27 18:22:17 -0500

    add edit toggle key

--- main.py
+++ main.py
@@ -12,9 +12,16 @@ class App(QObject):
 
     def __init__(self):
         super().__init__()
+
+        with open('res/settings.json', 'r') as f:
+            settings = json.load(f)
+
+        self.toggle_key = settings.get('toggle_key', '`')
+        keyboard.add_hotkey(self.toggle_key, self.toggle_windows, suppress=True)
+
         self.windows = [
-            Info((10, 10, 200, 1)),
-            ColorPicker((10, 120, 200, 1)),
+            Info((10, 10, 200, 1), set_toggle_key=self.set_toggle_key, key=self.toggle_key),
+            ColorPicker((10, 150, 200, 1)),
             RunCmd((220, 10, 200, 1))
         ]
         self.is_hidden = False
@@ -23,16 +30,24 @@ class App(QObject):
             self.toggle.connect(window.toggle_windows)
             window.show()
 
-        with open('res/settings.json', 'r') as f:
-            settings = json.load(f)
-        
-        toggle_key = settings.get('toggle_key', '`')
-        print(f"Toggle key: {toggle_key}")
-        keyboard.add_hotkey(toggle_key, self.toggle_windows, suppress=True)
-
     def toggle_windows(self):
         self.toggle.emit(self.is_hidden)
         self.is_hidden = not self.is_hidden
+
+    def set_toggle_key(self, key):
+        print(f"Changing toggle key from {self.toggle_key} to {key}")
+        if self.toggle_key == key:
+            return
+
+        keyboard.remove_hotkey(self.toggle_key)
+        self.toggle_key = key
+
+        keyboard.add_hotkey(self.toggle_key, self.toggle_windows, suppress=True)
+        with open('res/settings.json', 'r') as f:
+            settings = json.load(f)
+        settings['toggle_key'] = key
+        with open('res/settings.json', 'w') as f:
+            json.dump(settings, f, indent=4)
 
 
 if __name__ == "__main__":
--- windows/info.py
+++ windows/info.py
@@ -1,32 +1,58 @@
 import sys
 
+import keyboard
 from PySide6.QtCore import QUrl
 from PySide6.QtGui import QDesktopServices
-from PySide6.QtWidgets import QPushButton, QHBoxLayout
+from PySide6.QtWidgets import QPushButton, QLabel
 
 from components.custom_window import CustomWindow
 import json
+from PySide6.QtWidgets import QGridLayout
 
 
 class Info(CustomWindow):
-    def __init__(self, geometry):
-        super().__init__("Info", geometry)
+    def __init__(self, geometry, set_toggle_key=None, key='`'):
+        super().__init__('Info', geometry)
 
-        self.ver = QPushButton(f"v{self.get_version(self)}")
+        self.grid_layout = QGridLayout()
+        self.set_toggle_key = set_toggle_key
+
+        self.ver = QPushButton(f'v{self.get_version(self)}')
         self.ver.clicked.connect(self.open_url)
-        self.layout.addWidget(self.ver)
+        self.grid_layout.addWidget(self.ver, 0, 0, 1, 2)
 
-        self.startup = QPushButton("Startup App")
+        self.set_key = QPushButton('Edit Toggle Key')
+        self.set_key.setCheckable(True)
+        self.set_key.clicked.connect(self.select_key)
+        self.grid_layout.addWidget(self.set_key, 1, 0)
+
+        self.key_label = QLabel(' Key = ' + key)
+        self.grid_layout.addWidget(self.key_label, 1, 1)
+
+        self.startup = QPushButton('Startup App')
         self.startup.setCheckable(True)
         self.startup.clicked.connect(self.add_startup)
+        self.grid_layout.addWidget(self.startup, 3, 0)
 
-        self.q = QPushButton("Quit")
+        self.q = QPushButton('Quit')
         self.q.clicked.connect(self.quit)
+        self.grid_layout.addWidget(self.q, 3, 1)
 
-        self.h_layout = QHBoxLayout()
-        self.h_layout.addWidget(self.startup, stretch=2)
-        self.h_layout.addWidget(self.q , stretch=1)
-        self.layout.addLayout(self.h_layout)
+        self.grid_layout.setColumnStretch(0, 3)
+        self.grid_layout.setColumnStretch(1, 1)
+        self.layout.addLayout(self.grid_layout)
+
+    def select_key(self):
+        def on_key_press(event):
+            key = event.name
+            self.set_key.setChecked(False)
+            self.key_label.setText(f' Key = {key}')
+            self.set_toggle_key(key)
+            keyboard.unhook(self.key_hook)
+
+        self.set_key.setChecked(True)
+        self.set_key.setText('Press a key')
+        self.key_hook = keyboard.on_press(on_key_press)
 
     @staticmethod
     def get_version(self):
@@ -37,15 +63,15 @@ class Info(CustomWindow):
 
     @staticmethod
     def open_url(self):
-        QDesktopServices.openUrl(QUrl("https://github.com/LeoCh01/windows-helper"))
+        QDesktopServices.openUrl(QUrl('https://github.com/LeoCh01/windows-helper'))
 
     @staticmethod
     def quit(self):
-        print("Quitting")
+        print('Quitting')
         sys.exit()
 
     def add_startup(self):
         if self.startup.isChecked():
-            print("Adding to startup")
+            print('Adding to startup')
         else:
-            print("Removing from startup")
+            print('Removing from startup')
