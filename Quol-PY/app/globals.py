46fac911d48ae3b47797ee6f56ae9cc45d4d3b33
Leo Cheng <leocheng333375749@gmail.com>
2026-07-31 14:01:37 -0400

    stop Quol-PY tracking

--- Quol-PY/app/globals.py
+++
@@ -1,6 +0,0 @@
-BASE_DIR: str = None
-
-
-def set_dir(s: str):
-    global BASE_DIR
-    BASE_DIR = s
--- Quol-PY/app/transitions/left/transition.py
+++
@@ -1,8 +0,0 @@
-from qlib.windows.quol_window import QuolBaseWindow
-from qlib.transitions.transition_loader import TransitionInfo
-from qlib.transitions.move_transition import MoveTransition
-
-
-class Transition(MoveTransition):
-    def __init__(self, transition_info: TransitionInfo, window: QuolBaseWindow):
-        super().__init__(transition_info, window, 'left')
